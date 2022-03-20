168657847d3a97d76b40552e778401d893f8b070
ivanbahutski <ivanbogutski1995@gmail.com>
2022-03-20 14:05:53 +0100

    Configured posrgres db

--- app/core/admin.py
+++ app/core/admin.py
@@ -10,19 +10,19 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('name',)}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
-            _('Permissions'),
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+            _('Permissions'), {
+                'fields': ('is_active', 'is_staff', 'is_superuser')
+            }
         ),
         (_('Important dates'), {'fields': ('last_login',)})
     )
-
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'password1', 'password2')
-        })
+        }),
     )
 
 
